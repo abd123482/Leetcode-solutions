3c57175cd835f760a172bc73d46708813bd7bc13
NAS.rO <abdkkassl@gmail.com>
2025-09-06 01:16:16 +0200

    Create 136-Single-Number.c

---
+++ 136-Single-Number.c
@@ -0,0 +1,13 @@
+int singleNumber(int* nums, int numsSize) {
+    for (int i = 0; i < numsSize; i++) {
+        int flag = 0;
+        for (int j = 0; j < numsSize; j++) {
+            if (i != j && nums[i] == nums[j]) {
+                flag = 1;
+                break;
+            }
+        }
+        if (!flag) return nums[i];
+    }
+    return -1; 
+}
